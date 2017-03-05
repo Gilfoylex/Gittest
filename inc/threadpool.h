a0f7286799677271c392f176eb8771e955379106
cqtoracle <183849482@qq.com>
2017-03-05 22:20:01 +0800

    Update threadpool.h
    添加注释

--- inc/threadpool.h
+++ inc/threadpool.h
@@ -9,21 +9,20 @@
 #include <assert.h>
 #include <stdio.h>
 #include <signal.h>
-#include <dlfcn.h>
+#include <dlfcn.h>	//动态加载共享库
 #include "data.h"
 
 
 class MyTask
 {
-	public:
-		int run(int data);
+public:
+	int run(int data); //不同的任务可以键不同的类，通过std::function<void()>将任务抽象为可执行函数
 };
 
 class ThreadPool
 {
 public:
-	typedef std::function<void()> Task;
-	
+	typedef std::function<void()> Task;	//任务对象，具体参见任务实现在源文件	
 	ThreadPool(int threadNum = 10);
 	~ThreadPool();
 
@@ -37,7 +36,7 @@ private:
 	static void* threadFunc(void * threadData);
 
 
-	//ban operator =
+	//禁用默认的两个函数	
 	ThreadPool& operator = (const ThreadPool&);
 	ThreadPool(const ThreadPool&);
 
